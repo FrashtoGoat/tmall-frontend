01fe2fe106ed26351b1027e191a9ffb2791eb0fd
echo <1271573554@qq.com>
2021-02-07 23:27:14 +0800

    主体页面

--- statics/myjs/index.js
+++ statics/myjs/index.js
@@ -2,6 +2,41 @@
  var app = new Vue({
      el:"#box",
      data:{
-         msg:"tmall商城",
+         title:"mymall商城",
+         showIndex:false,
+         showProduct:false,
+         showMine:true,
+         currentPage:0,
+     },
+     methods:{
+        showPage(flag){
+
+            if(flag!=this.currentPage){
+                switch(flag){
+                    case 0:
+                        this.showProduct=false;
+                        this.showMine=false;
+                        this.showIndex=true;
+                        break;
+                    case 1:
+                        this.showIndex=false;
+                        this.showMine=false;
+                        this.showProduct=true;
+                        break;
+                    case 2:
+                        this.showProduct=false;
+                        this.showIndex=false;
+                        this.showMine=true;
+                        break;
+                    default:
+                        this.showProduct=false;
+                        this.showMine=false;
+                        this.showIndex=true;
+                }
+                this.currentPage=flag;
+            }
+            
+        },
+
      }
  });
